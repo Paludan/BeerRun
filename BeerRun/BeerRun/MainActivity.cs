fe5d622db20eaf5d5b6ac60706efb13998d21627
Tobias Morell <tmorel14@student.aau.dk>
2015-11-04 13:42:42 +0100

    ready for coding!

--- BeerRun/BeerRun/MainActivity.cs
+++ BeerRun/BeerRun/MainActivity.cs
@@ -19,9 +19,6 @@ namespace BeerRun
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
 		}
 	}
 }
